13900f13fa4ce0e5294ab181f348a74c3f4f733a
Vladislav <vladislav.romaschenko@saritasa.com>
2023-11-20 12:55:01 +0500

    Update docstring

--- provision/docker.py
+++ provision/docker.py
@@ -87,7 +87,9 @@ def stop(context):
 @task
 def clear(context):
     """Stop and remove all containers defined in docker-compose.
+
     Also remove images.
+
     """
     common.success("Clearing docker compose")
     context.run("docker compose rm -f")
