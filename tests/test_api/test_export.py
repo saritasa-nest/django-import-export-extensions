9100a07f13111ee011a176f7d0b5022342fcb1d4
Vladislav <vladislav.romaschenko@saritasa.com>
2023-12-04 18:31:47 +0500

    Refactor tests

--- tests/test_api/test_export.py
+++ tests/test_api/test_export.py
@@ -1,6 +1,7 @@
-from django.contrib.auth.models import User
-from django.test.client import Client
 from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.test import APIClient
 
 import pytest
 
@@ -8,34 +9,30 @@ from import_export_extensions.models import ExportJob
 
 
 @pytest.mark.django_db(transaction=True)
-def test_export_api_creates_export_job(client: Client, superuser: User):
+def test_export_api_creates_export_job(admin_api_client: APIClient):
     """Ensure export start API creates new export job."""
-    client.force_login(superuser)
-
-    response = client.post(
+    response = admin_api_client.post(
         path=reverse("export-artist-start"),
         data={
             "file_format": "csv",
         },
     )
+    assert response.status_code == status.HTTP_201_CREATED
     assert response.data["export_status"] == "CREATED"
     assert ExportJob.objects.first()
 
 
 @pytest.mark.django_db(transaction=True)
 def test_export_api_detail(
-    client: Client,
+    admin_api_client: APIClient,
     artist_export_job: ExportJob,
-    superuser: User,
 ):
     """Ensure export detail API shows current export job status."""
-    client.force_login(superuser)
-
-    response = client.get(
+    response = admin_api_client.get(
         path=reverse(
             "export-artist-detail",
             kwargs={"pk": artist_export_job.id},
         ),
     )
-
+    assert response.status_code == status.HTTP_200_OK
     assert response.data["export_finished"]
