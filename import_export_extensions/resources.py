d0ba35f15a4c87b19222223fc4177a9ff8a7d63b
Eg0ra <egor.toryshak@saritasa.com>
2024-12-19 14:35:03 +0700

    Improve docstring for update_task_status

--- import_export_extensions/resources.py
+++ import_export_extensions/resources.py
@@ -266,17 +266,29 @@ class CeleryResourceMixin:
     ):
         """Update state of the current event.
 
-        Receives meta of the current task and increase the `current`.
+        Receives meta of the current task and increase the `current`. Task
+        state is updated when current item is a multiple of
+        `self.status_update_row_count` or equal to  total number of items.
+
+        For example: once every 1000 objects (if the current object is 1000,
+        2000, 3000) or when current object is the last object, in order to
+        complete the import/export.
+
+        This needed to increase the speed of import/export by reducing number
+        of task status updates.
 
         """
         if not current_task or current_task.request.called_directly:
             return
 
         self.current_object_number += 1
-        if (
+
+        is_reached_update_count = (
             self.current_object_number % self.status_update_row_count == 0
-            or self.current_object_number == self.total_objects_count
-        ):
+        )
+        is_last_object = self.current_object_number == self.total_objects_count
+
+        if is_reached_update_count or is_last_object:
             self._update_current_task_state(
                 state=state,
                 meta={
